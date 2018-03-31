9851df8e186e8ea82f25c6af28bdd460882217e6
YassineAlo <m.yassine.aloui@gmail.com>
2018-04-01 00:55:34 +0200

    Add PATCH /todos/id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -7,10 +7,12 @@ const {Todo} = require('./../models/todo');
 
 const todos = [{
     _id: new ObjectID(),
-    text: 'First to do'
+    text: 'First to do',
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -153,5 +155,52 @@ describe('DELETE /todos/:id', () => {
                 .delete(`/todos/123`)
                 .expect(404)
                 .end(done);
+    });
+});
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        var hexId = todos[0]._id.toHexString();
+        var text = 'This should be the new text';
+
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: true,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                // expect(res.body.todo.completedAt).toBeA('number');  deprecated
+            })
+            .end(done);
+
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        var hexId = todos[1]._id.toHexString();
+        var text = 'This should be the new text!';
+
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: false,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                // expect(res.body.todo.completedAt).toNotExist(); deprecated
+            })
+            .end(done);
+        // grab id of seconde todo
+        // update text to smth different set completed to false
+        //200
+        // text is changed to whatever, completed is false, completedAt is null .toNotExist
     });
 });
