7f6941f107caa4920988704c2edd894368c45277
YassineAlo <m.yassine.aloui@gmail.com>
2018-03-31 16:11:53 +0200

    Add GET /todos/id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -27,6 +28,35 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+// GET /todos/id
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    // isValid
+    if (!ObjectID.isValid(id)) {
+        console.log('Invalid ObjectID');
+        return res.status(404).send();
+    }
+    
+    Todo.findById(id).then((todo) => {
+        if (todo) {
+            res.status(200).send({todo});
+        } else {
+            res.status(404).send();
+            console.log('Unable to find the oject with id: ', id);
+        }
+    }).catch((e) => {
+        res.status('404').send();
+        console.log('An error as occurred while fetching the data')
+    });
+    // findById
+        //success
+            // if todo - send it back
+            // if no todo - send back 404 with empty body
+        //error
+            //404 - send back nothing
+    
 });
 
 
