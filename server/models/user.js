89d29191b1d227c84d8f3ecb728ecd832ea3f53e
YassineAlo <m.yassine.aloui@gmail.com>
2018-04-08 22:26:34 +0200

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,38 @@ UserSchema.statics.findByToken = function (token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+    
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+            if (res) {
+                resolve(user);
+            } else {
+                reject();
+            };
+            })
+        }) 
+    });
+    // User.findOne({email: body.email}).then((doc) => {
+    //     var testPass = bcrypt.compare(body.password, res.body.password, (err, res) => {
+    //         console.log(res);
+    //     });
+
+    //     if (testPass) {
+    //         res.status(200).send(doc);
+    //     } else {
+    //         res.status(401).send();
+    //         console.log('Acces denied');
+    //     }
+    // });
+};
+
 UserSchema.pre('save', function (next) {
     var user = this;
 
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 var express = require('express');
 var bodyParser = require('body-parser');
 const _ = require('lodash')
+const bcrypt = require('bcryptjs');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -117,7 +118,19 @@ app.post('/users' , (req, res) => {
     });
 });
 
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
 
+    User.findByCredentials(body.email, body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+});
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
